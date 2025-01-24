41eeef9bbc43d5dd1da02670f5a3e6010fce090f
Orangerot <purple@orangerot.dev>
2025-01-25 00:48:55 +0100

    feat: parallax der lachs

--- main.lua
+++ main.lua
@@ -2,17 +2,25 @@ require("lib/spritesheet")
 height = 256
 width = 320
 scale = 1
+puffy_x = 0
 
 function love.load()
   background = love.graphics.newImage("assets/environment/background.png")
+  background:setWrap('repeat', 'repeat')
+  midground = love.graphics.newImage("assets/environment/midground.png")
+  midground:setWrap('repeat', 'repeat')
   puffy = newAnimation(love.graphics.newImage("assets/puffy.png"), 200, 200, 1.0)
 
+  local width, height = love.graphics.getDimensions()
+  local background_width, background_height = background:getDimensions()
+  local aspect_ratio = background_width / background_height
+  love.window.setMode(height * aspect_ratio, height)
   love.resize(love.graphics.getDimensions())
   is_puffy_blow = false
 end
 
 function love.update()
-  
+  puffy_x = puffy_x + 1
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -32,7 +40,13 @@ end
 
 function love.draw()
   love.graphics.scale(scale)
-  love.graphics.draw(background, 0, 0)
+  local background_width, background_height = background:getDimensions()
+  local background_quad = love.graphics.newQuad(puffy_x * 0.1, 0, background_width, background_height, background_width, background_height)
+  love.graphics.draw(background, background_quad)
+  local midground_width, midground_height = midground:getDimensions()
+  local midground_quad = love.graphics.newQuad(puffy_x * 0.5, 0, midground_width, midground_height, midground_width, midground_height)
+  local midground_scale = background_height / midground_height
+  love.graphics.draw(midground, midground_quad, 0, 0, 0, midground_scale, midground_scale)
   love.graphics.scale(.2)
   if is_puffy_blow then
       love.graphics.draw(puffy.spriteSheet, puffy.quads[1])
